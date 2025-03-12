5c34e3904287cb382ae493861ceab217e43725ad
nicky goodman <nickyggoodman@gmail.com>
2025-03-12 11:19:09 -0400

    testing requests module

--- rym-spotify-sync.py
+++ rym-spotify-sync.py
@@ -1,26 +1,57 @@
 '''
 Author: Nicholas G Goodman
 
-This script syncronizes a user's Rate Your Music (RYM) ratings with Spotify by 
-adding each album to a Spotify playlist based on the rating it was given by the 
+This script syncronizes a user's Rate Your Music (RYM) ratings with Spotify by
+adding each album to a Spotify playlist based on the rating it was given by the
 user in RYM. A total of 11 playlists are made. One for unrated albums (albums
 with a rating of 0) and ten for albums rating 1 through 10.
 '''
 
 import sys
 import csv
+import string
+import secrets  # "suitable for managing data such as [...] account auth"
+import hashlib
+import base64
+import webbrowser
+import requests
+
 
 BASE_URL = 'https://api.spotify.com'
 
 REDIRECT_URL = 'http://localhost:8080'
-CLIENT_ID = ''
+CLIENT_ID = '58a65635db43470fa773cba91b820b49'
 
 AUTHORIZATION_ENDPOINT = 'https://accounts.spotify.com/authorize'
 TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
 SCOPE = 'user-read-private user-read-email'
 
+
+def generate_random_string(length):
+    possible = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(possible) for i in range(length))
+
+
+def sha256(plain):
+    m = hashlib.sha256()
+    m.update(plain.encode())
+    return m.digest()
+
+
 if __name__ == "__main__":
-    with  open(sys.argv[1], newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            print(row['Rating'])
+    code_verifier = generate_random_string(64)
+    hashed = sha256(code_verifier)
+    code_challenge = base64.b64encode(hashed)
+    payload = {
+            'response_type': 'code',
+            'client_id': CLIENT_ID,
+            'scope': SCOPE,
+            'code_challenge_method': 'S256',
+            'code_challenge': code_challenge,
+            'redirect_uri': REDIRECT_URL
+            }
+    r = requests.get(AUTHORIZATION_ENDPOINT, params=payload)
+    # print(r.text)
+    # print(r.url)
+    print(r.history)
+
